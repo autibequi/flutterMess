8ef563bf1f112a0ced26f1d5b4a81768ec274cf8
pedro.correa@concrete.com.br <pedro.correa@concrete.com.br>
2019-05-30 17:45:01 -0300

    Adds simple counter.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import './product_manager.dart';
+import './counter.dart';
 
 // Instantiate the Main AppClass
 void main() => runApp(MyApp());
@@ -10,15 +11,17 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-        primarySwatch: Colors.pink,
-      ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('EasyList'),
+        theme: ThemeData(
+          primarySwatch: Colors.purple,
         ),
-        body: ProductManager('Food Tester'),
-      ),
-    );
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text('Purple Solutions'),
+          ),
+          body: Column(children: [
+            CounterManager(),
+            ProductManager('Food Tester'),
+          ]),
+        ));
   }
 }
