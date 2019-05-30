dd0714715865509c56dbabcb88d8b274a2c996d2
pedro.correa@concrete.com.br <pedro.correa@concrete.com.br>
2019-05-30 14:46:08 -0300

    Adds collor accent

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,17 @@ import 'package:flutter/material.dart';
 
 import './product_manager.dart';
 
+// Instantiate the Main AppClass
 void main() => runApp(MyApp());
 
+// Main App Class
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.pink,
+      ),
       home: Scaffold(
         appBar: AppBar(
           title: Text('EasyList'),
