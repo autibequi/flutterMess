1fbbbcf6a883c12cf065732ca9d16d04fc0d61f5
pedro.correa@concrete.com.br <pedro.correa@concrete.com.br>
2019-06-02 15:06:04 -0300

    Add tabs.

--- lib/pages/product_admin.dart
+++ lib/pages/product_admin.dart
@@ -1,18 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_mess/product_manager.dart';
 
 import './drawer.dart';
+
+import './product_create.dart';
+import './products.dart';
 
 class ProductAdminPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        drawer: DrawerManager(),
-        appBar: AppBar(
-          title: Text('Product Admin'),
-        ),
-        body: Container(
-          child: Center(
-            child: Text('PRODUCT MANAGER HERE'),
+    return DefaultTabController(
+        length: 2,
+        child: Scaffold(
+          drawer: DrawerManager(),
+          appBar: AppBar(
+            title: Text('Product Admin'),
+            bottom: TabBar(
+              tabs: <Widget>[
+                Tab(
+                  icon: Icon(Icons.create),
+                  text: 'Create Product',
+                ),
+                Tab(
+                  icon: Icon(Icons.list),
+                  text: 'Manage Products',
+                ),
+              ],
+            ),
+          ),
+          body: TabBarView(
+            children: <Widget>[
+              ProductCreatePage(),
+              ProductManager(),
+            ],
           ),
         ));
   }
