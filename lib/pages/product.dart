4e584ba7e3cabfd618743b2b4c9869cd89d7d2a3
pedro.correa@concrete.com.br <pedro.correa@concrete.com.br>
2019-06-02 13:37:48 -0300

    Add custom willpopscope controller

--- lib/pages/product.dart
+++ lib/pages/product.dart
@@ -8,22 +8,28 @@ class ProductPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(product['title']),
-      ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: <Widget>[
-          Image.asset(product['image']),
-          Container(padding: EdgeInsets.all(10.0), child: Text('Details!')),
-          RaisedButton(
-            child: Text('Delete!'),
-            color: Colors.red,
-            onPressed: () => Navigator.pop(context, index),
-          )
-        ],
-      ),
-    );
+    return WillPopScope(
+        onWillPop: () {
+          print('heuhue');
+          Navigator.pop(context, false);
+          return Future.value(false);
+        },
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text(product['title']),
+          ),
+          body: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Image.asset(product['image']),
+              Container(padding: EdgeInsets.all(10.0), child: Text('Details!')),
+              RaisedButton(
+                child: Text('Delete!'),
+                color: Colors.red,
+                onPressed: () => Navigator.pop(context, index),
+              )
+            ],
+          ),
+        ));
   }
 }
