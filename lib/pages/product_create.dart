9f36cdd1d695564c64116a0369dd7a6cb4f195f5
pedro.correa@concrete.com.br <pedro.correa@concrete.com.br>
2019-06-03 21:59:29 -0300

    Improves product add form.

--- lib/pages/product_create.dart
+++ lib/pages/product_create.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 
 class ProductCreatePage extends StatefulWidget {
@@ -6,20 +8,66 @@ class ProductCreatePage extends StatefulWidget {
 }
 
 class _ProductCreatePageState extends State<ProductCreatePage> {
-  String variabelText = 'placeholder';
+  String title;
+  String description;
+  double price;
+  bool isCool = false;
+
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        TextField(
-          onChanged: (String value) {
-            setState(() {
-              variabelText = value;
-            });
-          },
-        ),
-        Text(variabelText)
-      ],
-    );
+    return Container(
+        padding: EdgeInsets.all(10.0),
+        child: ListView(
+          children: <Widget>[
+            TextField(
+              decoration: InputDecoration(labelText: 'Product Name'),
+              onChanged: (String value) {
+                setState(() {
+                  title = value;
+                });
+              },
+            ),
+            TextField(
+              decoration: InputDecoration(
+                labelText: 'Product Description',
+              ),
+              maxLines: 4,
+              onChanged: (String value) {
+                setState(() {
+                  description = value;
+                });
+              },
+            ),
+            TextField(
+              decoration: InputDecoration(labelText: 'Product Price'),
+              keyboardType: TextInputType.number,
+              onChanged: (String value) {
+                setState(() {
+                  price = double.parse(value);
+                });
+              },
+            ),
+            SwitchListTile(
+              value: isCool,
+              onChanged: (bool value) {
+                setState(() {
+                  isCool = value;
+                });
+              },
+              title: Text('is it cool?'),
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            RaisedButton(
+              color: Theme.of(context).primaryColor,
+              child: Text('Submit'),
+              onPressed: () {
+                // TODO: Add new Product
+                // TODO: Go to Listing
+              },
+            )
+          ],
+        ));
   }
 }
