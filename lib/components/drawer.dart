880460f671a36a17ea1db74a7e944555b0cbba4a
pedro.correa@concrete.com.br <pedro.correa@concrete.com.br>
2019-06-04 12:37:20 -0300

    Adds icons to drawer

--- lib/components/drawer.dart
+++ lib/components/drawer.dart
@@ -8,11 +8,13 @@ class DrawerManager extends StatelessWidget {
         children: <Widget>[
           AppBar(automaticallyImplyLeading: false, title: Text('Choose')),
           ListTile(
+              leading: Icon(Icons.add),
               title: Text('Manage Products'),
               onTap: () {
                 Navigator.pushNamed(context, 'productAdmin');
               }),
           ListTile(
+              leading: Icon(Icons.list),
               title: Text('List Products'),
               onTap: () {
                 Navigator.pushNamed(context, 'productList');
