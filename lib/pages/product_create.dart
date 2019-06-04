d996e9d6cbecb2cd3d463688151c51230ff999d9
pedro.correa@concrete.com.br <pedro.correa@concrete.com.br>
2019-06-04 19:44:42 -0300

    Adds validation.

--- lib/pages/product_create.dart
+++ lib/pages/product_create.dart
@@ -2,6 +2,13 @@ import 'dart:developer';
 
 import 'package:flutter/material.dart';
 
+bool _isNumeric(String str) {
+  if (str == null) {
+    return false;
+  }
+  return double.tryParse(str) != null;
+}
+
 class ProductCreatePage extends StatefulWidget {
   final Function addProduct;
 
@@ -16,42 +23,58 @@ class _ProductCreatePageState extends State<ProductCreatePage> {
   String description;
   double price;
   bool isCool = false;
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
   Widget _buildProductNameField() {
-    return TextField(
-      decoration: InputDecoration(labelText: 'Product Name'),
-      onChanged: (String value) {
-        setState(() {
-          title = value;
+    return TextFormField(
+        decoration: InputDecoration(labelText: 'Product Name'),
+        validator: (String value) {
+          if (value.isEmpty || value.length < 5) {
+            return 'Title is required';
+          }
+        },
+        onSaved: (String value) {
+          setState(() {
+            title = value;
+          });
         });
-      },
-    );
   }
 
   Widget _buildProductDescriptionField() {
-    return TextField(
-      decoration: InputDecoration(
-        labelText: 'Product Description',
-      ),
-      maxLines: 4,
-      onChanged: (String value) {
-        setState(() {
-          description = value;
+    return TextFormField(
+        decoration: InputDecoration(
+          labelText: 'Product Description',
+        ),
+        maxLines: 4,
+        validator: (String value) {
+          if (value.isEmpty || value.length < 5) {
+            return 'Description is required';
+          }
+        },
+        onSaved: (String value) {
+          setState(() {
+            description = value;
+          });
         });
-      },
-    );
   }
 
   Widget _buildProductPriceField() {
-    return TextField(
-      decoration: InputDecoration(labelText: 'Product Price'),
-      keyboardType: TextInputType.number,
-      onChanged: (String value) {
-        setState(() {
-          price = double.parse(value);
+    return TextFormField(
+        decoration: InputDecoration(labelText: 'Product Price'),
+        keyboardType: TextInputType.number,
+        validator: (String value) {
+          if (value.isEmpty) {
+            return 'Price is required';
+          }
+          if (!_isNumeric(value)) {
+            return 'Price must be a number.';
+          }
+        },
+        onSaved: (String value) {
+          setState(() {
+            price = double.parse(value);
+          });
         });
-      },
-    );
   }
 
   Widget _buildIsItCoolSwitch() {
@@ -70,30 +93,36 @@ class _ProductCreatePageState extends State<ProductCreatePage> {
   Widget build(BuildContext context) {
     return Container(
         padding: EdgeInsets.all(10.0),
-        child: ListView(
-          children: <Widget>[
-            _buildProductNameField(),
-            _buildProductDescriptionField(),
-            _buildProductPriceField(),
-            _buildIsItCoolSwitch(),
-            SizedBox(
-              height: 20.0,
-            ),
-            RaisedButton(
-              color: Theme.of(context).primaryColor,
-              child: Text('Submit'),
-              onPressed: () {
-                Map newProduct = {
-                  'title': title,
-                  'description': description,
-                  'price': price,
-                  'image': 'assets/food.jpg',
-                };
-                widget.addProduct(newProduct);
-                Navigator.pushReplacementNamed(context, 'productList');
-              },
-            )
-          ],
-        ));
+        child: Form(
+            key: _formKey,
+            child: ListView(
+              children: <Widget>[
+                _buildProductNameField(),
+                _buildProductDescriptionField(),
+                _buildProductPriceField(),
+                _buildIsItCoolSwitch(),
+                SizedBox(
+                  height: 20.0,
+                ),
+                RaisedButton(
+                  color: Theme.of(context).primaryColor,
+                  child: Text('Submit'),
+                  onPressed: () {
+                    if (!_formKey.currentState.validate()) {
+                      return; // stops if validation is false
+                    }
+                    _formKey.currentState.save();
+                    Map newProduct = {
+                      'title': title,
+                      'description': description,
+                      'price': price,
+                      'image': 'assets/food.jpg',
+                    };
+                    widget.addProduct(newProduct);
+                    Navigator.pushReplacementNamed(context, 'productList');
+                  },
+                )
+              ],
+            )));
   }
 }
