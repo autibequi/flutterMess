b59dc7cb0f67660ac8cf6d814a0ee24694ec6cbd
pedro.correa@concrete.com.br <pedro.correa@concrete.com.br>
2019-06-04 11:32:09 -0300

    simplify product manager

--- lib/product_manager.dart
+++ lib/product_manager.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 
-import './products.dart';
-
 class ProductManager extends StatelessWidget {
   final List<Map> products;
   final Function deleteProduct;
@@ -14,5 +12,66 @@ class ProductManager extends StatelessWidget {
     return Column(
       children: [Expanded(child: Products(products, deleteProduct))],
     );
+  }
+}
+
+class Products extends StatelessWidget {
+  final List<Map> products;
+  final Function deleteProduct;
+
+  Products(this.products, this.deleteProduct);
+
+  Widget _buildProductItem(BuildContext context, int index) {
+    return Card(
+      child: Column(
+        children: <Widget>[
+          Image.asset(products[index]['image']),
+          Container(
+            padding: EdgeInsets.all(10.0),
+            child: Text(
+              products[index]['title'],
+              style: TextStyle(
+                  fontSize: 26.0,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'remachine'), // TODO: make it works!
+            ),
+          ),
+          Text(products[index]['description']),
+          ButtonBar(
+            alignment: MainAxisAlignment.center,
+            children: <Widget>[
+              FlatButton(
+                  child: Text('Details'),
+                  onPressed: () => Navigator.pushNamed<bool>(
+                              context, '/product/' + index.toString())
+                          .then((bool value) {
+                        if (value) {
+                          deleteProduct(index);
+                        }
+                      }))
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildProductList() {
+    Widget productCards;
+    if (products.length > 0) {
+      productCards = ListView.builder(
+        itemBuilder: _buildProductItem,
+        itemCount: products.length,
+      );
+    } else {
+      productCards = Container();
+    }
+    return productCards;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    print('[Products Widget] build()');
+    return _buildProductList();
   }
 }
